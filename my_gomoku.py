b16bd65241cd22ae993a072dbb2f4611dcffac78
somaknzw <sknzw2608@gmail.com>
2018-09-20 16:07:02 +0900

    submit_code

--- my_gomoku.py
+++ my_gomoku.py
@@ -8,59 +8,166 @@ N = 15
 def Think(field):
     CENTER = (int(N / 2), int(N / 2))
     best_position = (0, 0)
-    for i in range(N):
-        for j in range(N):
+    for i in reversed(range(N)):
+        for j in reversed(range(N)):
             if field[i][j] != '.':
                 continue
 
+            # ポジション設定
             position = (i, j)
 
+            # ごれんちゃんが完成するかを判定、そうでなかったら中心に近いところにおく
             # Assume to put a stone on (i, j).
             field[i][j] = 'O'
 
-            #ごれんちゃんが完成するかを判定、そうでなかったら中心に近いところにおく
-            if CanHaveFiveStones(field, position,5):
+            if CanHaveFiveStones(field, position, 5):
                 DebugPrint('I have a winning choice at (%d, %d)' % (i, j))
+
+                DebugPrint('tag A', i, j)
+                field[i][j] = '.'
                 return position
+            else:
+                field[i][j] = '.'
+                continue
+
+
+    for i in reversed(range(N)):
+        for j in reversed(range(N)):
+            if field[i][j] != '.':
+                continue
+
+
+            # ポジション設定
+            position = (i, j)
+
+            field[i][j] = 'X'
+            if CanHaveFiveEnemyStones(field, position, 5):
+                # すでに４つ並んでる場合
+                DebugPrint('tag B', i, j)
+                field[i][j] = '.'
+                return position
+            else:
+                field[i][j] = '.'
+
+    for i in reversed(range(N)):
+        for j in reversed(range(N)):
+            if field[i][j] != '.':
+                continue
+
+            # ポジション設定
+            position = (i, j)
+
+            # 相手の状態を判定
+            field[i][j]='X'
+            if CanHaveFiveEnemyStones(field, position, 4):
+                # 3つ並んでる場合
+                DebugPrint('tag C', i, j)
+                field[i][j] = '.'
+
+                # # -----
+                # hanteiList = []
+                # hanteiList = getHantei(field, position)
+                # DebugPrint('Listhantei', hanteiList)
+                # for x in hanteiList:
+                #     if x != None:
+                #         return x
+                #         break
+                # # -----
+                return position
+            # elif CanHaveFiveEnemyStones(field, position, 3):
+            #     # 2つ並んでる場合
+            #     # field[i][j]='O'
+            #     DebugPrint('tag H', i, j)
+            #     field[i][j] = '.'
+            #     return position
+
+            else:
+                field[i][j] = '.'
+
+    for i in reversed(range(N)):
+        for j in reversed(range(N)):
+            if field[i][j] != '.':
+                continue
+
+            # ポジション設定
+            position = (i, j)
+
+            # 自分の攻撃
+            # Assume to put a stone on (i, j).
+            field[i][j] = 'O'
             if CanHaveFiveStones(field, position,4):
+                DebugPrint('tag D', i, j)
+                field[i][j] = '.'
                 return position
-            if CanHaveFiveStones(field, position,3):
-                return position
+            elif CanHaveFiveStones(field, position,3):
+                # 3つ揃いそうなとき
+                DebugPrint('tag E', i, j)
+                hanteiList = getHantei(field, position)
+                DebugPrint(hanteiList)
+                if True in hanteiList and hanteiList.index(True) >= 1:
+                    field[i][j] = '.'
+                    return position
+                else:
+                    field[i][j] = '.'
+                    continue
             #if CanHaveFiveStones(field, position,2):
             #    return position
+            else:
+                field[i][j] = '.'
+                DebugPrint('tag F', i, j)
+                continue
 
+    for i in reversed(range(N)):
+        for j in reversed(range(N)):
+            if field[i][j] != '.':
+                continue
+
+            # ポジション設定
+            position = (i, j)
 
             # Revert the assumption.
             field[i][j] = '.'
             if GetDistance(best_position, CENTER) > GetDistance(position, CENTER):
                 best_position = position
+                DebugPrint('tag G', i, j)
     return best_position
 
 
 # Returns true if you have five stones from |position|. Returns false otherwise.
-def CanHaveFiveStones(field, position,n):
-    return (CountStonesOnLine(field, position, (1, 1)) >= n or
-            CountStonesOnLine(field, position, (1, 0)) >= n or
-            CountStonesOnLine(field, position, (1, -1)) >= n or
-            CountStonesOnLine(field, position, (0, 1)) >= n)
+def CanHaveFiveStones(field, position, num):
+    return (CountStonesOnLine(field, position, (1, 1), "O") >= num or
+            CountStonesOnLine(field, position, (1, 0), "O") >= num or
+            CountStonesOnLine(field, position, (1, -1), "O") >= num or
+            CountStonesOnLine(field, position, (0, 1), "O") >= num)
 
-def CanHaveFiveStonesonX(field, position,n):
-    return (CountXOnLine(field, position, (1, 1)) >= n or
-            CountXOnLine(field, position, (1, 0)) >= n or
-            CountXOnLine(field, position, (1, -1)) >= n or
-            CountXOnLine(field, position, (0, 1)) >= n)
+def CanHaveFiveEnemyStones(field, position, n):
+    return (CountStonesOnLine(field, position, (1, 1), 'X') >= n or
+            CountStonesOnLine(field, position, (1, 0), 'X') >= n or
+            CountStonesOnLine(field, position, (1, -1), 'X') >= n or
+            CountStonesOnLine(field, position, (0, 1), 'X') >= n)
 
+def getHantei(field, position):
+    hanteiList = []
+
+    hanteiList.append(Hantei(field, position, (1, 1), 'O'))
+    hanteiList.append(Hantei(field, position, (1, 0), 'O'))
+    hanteiList.append(Hantei(field, position, (1, -1), 'O'))
+    hanteiList.append(Hantei(field, position, (0, 1), 'O'))
+
+    return hanteiList
 
 
 # Returns the number of stones you can put around |position| in the direction specified by |diff|.
-def CountStonesOnLine(field, position, diff):
+def CountStonesOnLine(field, position, diff, mark):
     count = 0
 
     row = position[0]
     col = position[1]
     while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != mark:
             break
+
+
         row += diff[0]
         col += diff[1]
         count += 1
@@ -68,46 +175,24 @@ def CountStonesOnLine(field, position, diff):
     row = position[0] - diff[0]
     col = position[1] - diff[1]
     while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != mark:
             break
         row -= diff[0]
         col -= diff[1]
         count += 1
 
+
     return count
 
-def CountXOnLine(field, position, diff):
+def Hantei(field, position, diff, mark):
     count = 0
-
-    row = position[0]
-    col = position[1]
-    while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'X':
-            break
-        row += diff[0]
-        col += diff[1]
-        count += 1
-
-    row = position[0] - diff[0]
-    col = position[1] - diff[1]
-    while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'X':
-            break
-        row -= diff[0]
-        col -= diff[1]
-        count += 1
-
-    return count
-    
-
-def Hantei(field, pisition, char):
-    count, countA
+    countA = 0
 
     row = position[0]
     col = position[1]
 
     while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != char or count == 4:
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != mark or count == 3:
             break
 
         row += diff[0]
@@ -119,7 +204,7 @@ def Hantei(field, pisition, char):
     col = position[1] - diff[1]
 
     while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != char or count == 4:
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != mark or count == 3:
             break
 
         row -= diff[0]
@@ -127,29 +212,43 @@ def Hantei(field, pisition, char):
         count += 1
 
     # 阻止か攻撃かを判定
-    if count == 4 and countA == 4:
+    if count == 3 and countA == 3:
         # 阻止する
         row = position[0]
         col = position[1]
 
-        for x in range(4):
+        for x in range(3):
             row += diff[0]
             col += diff[1]
 
-        return (row, col)
+        if row < 0 or col < 0 or row >= N or col >= N:
+            return False
+        else:
+            if field[row][col] == 'X':
+                return False
+            else:
+                return True
 
-    elif count == 4 and countA == 1:
+    elif count == 3 and countA == 1:
         # 阻止する
-        row = position[0] - diff[0]
-        col = position[1] - diff[1]
+        row = position[0]
+        col = position[1]
 
-        for x in range(4):
+        for x in range(3):
             row -= diff[0]
             col -= diff[1]
 
+        if row < 0 or col < 0 or row >= N or col >= N:
+            return False
+        else:
+            if field[row][col] == 'X':
+                return False
+            else:
+                return True
+
     else:
         # 阻止する必要なし
-        return None
+        return False
 
 
 # Returns the Manhattan distance from |a| to |b|.
